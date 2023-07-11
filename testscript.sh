37151677cfe5aecd614206be7dc09f3670d1b2ae
Diego Perez Gramond Muñoz <diego.perezgramond@softtekprojects.com>
2023-07-11 11:08:58 -0600

    fixed the broken condition

--- testscript.sh
+++ testscript.sh
@@ -4,7 +4,7 @@ echo "this script is from github"
 NUMBER=$(($NUMONE + $NUMTWO))
 echo "$NUMBER"
 echo "this is now working!"
-if [[ $NUM -eq 23 ]]
+if [[ $NUM == 23 ]]
 then
 	echo "This is the best number"
 fi
